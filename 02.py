cfcebadb4425476936099e729185d83c1dca4d98
sara-kassani <kassani.sara@gmail.com>
2021-08-15 09:16:05 -0600

    Add numbers entered by the users

---
+++ 02.py
@@ -0,0 +1,9 @@
+# Add numbers entered by the users (fixed)
+def main():
+    a = input('First number: ')
+    b = input('Second number: ')
+
+    print(int(a) + int(b))
+
+if __name__ == '__main__':
+    main()
