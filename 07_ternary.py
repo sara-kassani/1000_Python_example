80b30381c824270a148ce237e3040425b9437bba
sara-kassani <kassani.sara@gmail.com>
2021-08-16 08:15:01 -0600

    ternary operator

---
+++ 07_ternary.py
@@ -0,0 +1,22 @@
+# Ternary operator
+def main():
+    x = 3
+    answer = 'positive' if x > 0 else 'negative or zero'
+    print(answer)
+
+    x = -3
+    answer = 'positive' if x > 0 else 'negative or zero'
+
+# in other languages this is the ?: construct.
+
+###############################################################
+
+# x = 3
+# if x > 0:
+#     answer = 'positive'
+#     print('answer')
+#
+# else:
+#     answer = 'negative or zero'
+#     print('answer')
+
