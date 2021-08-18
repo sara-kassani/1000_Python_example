9f1f48a15d694f7056d7488bf36532f7be81b9ca
sara-kassani <kassani.sara@gmail.com>
2021-08-18 09:47:34 -0600

    cli

---
+++ 12_cli.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    print(sys.argv)
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+
+if __name__ == '__main__':
+    main()
+
+## run: 12_cli.py one two
