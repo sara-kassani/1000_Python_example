5f59e5af0fc1f7bb204fa32ce6438fb2bc0d1df2
sara-kassani <kassani.sara@gmail.com>
2021-08-19 10:44:33 -0600

    toggle

---
+++ 23_toggle.py
@@ -0,0 +1,20 @@
+"""
+toggle
+"""
+def main():
+    status_is_on = False
+    print(status_is_on)
+
+    status_is_on = not status_is_on
+    print(status_is_on)
+
+    status_is_on = not status_is_on
+    print(status_is_on)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
