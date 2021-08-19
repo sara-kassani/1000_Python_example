e4f46c1715e48ad36e3d520e75b98594a347352b
sara-kassani <kassani.sara@gmail.com>
2021-08-19 09:21:30 -0600

    exercise: fruit salad

---
+++ 20_fruit_salad.py
@@ -0,0 +1,14 @@
+"""
+Exercise: Fruit salad
+- write a script that will pick 3 fruits from a list of fruits and print them.
+- these fruits should be different.
+"""
+
+import random
+def main():
+    fruits = ['Apple', 'Banana', 'Peach', 'Orange', 'Durian', 'Papaya']
+    salad = random.sample(fruits, 3)
+    print(salad)
+
+if __name__ == '__main__':
+    main()
