87db8be9f1aef9fafbc102edaa428c7edc0e24c7
sara-kassani <kassani.sara@gmail.com>
2021-08-19 12:24:57 -0600

    string repetition and concatenation

---
+++ 01_string/02_str_rep_conact.py
@@ -0,0 +1,14 @@
+"""
+string repetition and concatenation
+"""
+def main():
+    divider = '### ' *15
+    print(divider)
+
+    h1 = 'hello'
+    h2 = 'world'
+    print(h1 + ' ' + h2)
+
+
+if __name__ == '__main__':
+    main()
