3a8701adc30338a2f224b7792ec6f421c487a8ab
sara-kassani <kassani.sara@gmail.com>
2021-08-19 12:24:01 -0600

    string length

---
+++ 01_string/01_str_len.py
@@ -0,0 +1,14 @@
+"""
+string length (len)
+"""
+def main():
+    line = 'hello world!'
+    print(len(line))
+
+    txt = """hello
+    world!"""
+    print(len(txt))
+
+if __name__ == '__main__':
+    main()
+
