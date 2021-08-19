6ab805772263e86e098c480134d15d656b45422b
sara-kassani <kassani.sara@gmail.com>
2021-08-19 09:17:45 -0600

    random choice

---
+++ 18_random_choice.py
@@ -0,0 +1,12 @@
+# Random choice
+
+import random
+def main():
+    letter = "abcdefghiljklmno"
+    print(random.choice(letter))
+
+    fruits = ["Apple", "Banana", "Peach", "Orange", "Durian", "Papaya"]
+    print(random.choice(fruits))
+
+if __name__ == "__main__":
+    main()
