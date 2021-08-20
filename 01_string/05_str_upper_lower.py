cb51fea9c4a52472ace57c5f78b514390700672d
sara-kassani <kassani.sara@gmail.com>
2021-08-20 08:44:38 -0600

    len, upper and lower

---
+++ 01_string/05_str_upper_lower.py
@@ -0,0 +1,17 @@
+"""
+string functions and methods (len, upper and lower)
+"""
+def main():
+    a = 'xYz'
+    print(len(a))
+    b = a.upper()
+
+    print(b)
+    print(a)  #immutable
+
+    print (a. lower())
+
+
+if __name__ == '__main__':
+    main()
+
