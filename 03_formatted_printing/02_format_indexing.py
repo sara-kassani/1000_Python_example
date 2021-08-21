751f470832455131a082d17c4c43a1bf987152a5
sara-kassani <kassani.sara@gmail.com>
2021-08-21 09:15:38 -0600

    string format indexing

---
+++ 03_formatted_printing/02_format_indexing.py
@@ -0,0 +1,18 @@
+"""
+Examples using format - indexing
+"""
+
+
+def main():
+    txt = "Foo Bar"
+    num = 42.12
+
+    print('The user {} was born {} years ago.'.format(txt, num))
+    print('The user {0} was born {1} years ago.'.format(txt, num))
+    print('The user {1} was born {0} years ago.'.format(num, txt))
+
+    print('{0} is {0} and {1} years old.'.format(txt, num))
+
+
+if __name__ == '__main__':
+    main()
