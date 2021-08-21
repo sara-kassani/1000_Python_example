2ccf4652b132ba92adb7629f36ae4ab9b1a48f18
sara-kassani <kassani.sara@gmail.com>
2021-08-21 09:16:45 -0600

    string formattimg with names

---
+++ 03_formatted_printing/03_format_names.py
@@ -0,0 +1,17 @@
+"""
+Examples using foramt with names
+"""
+
+
+def main():
+    txt = 'Foo Bar'
+    num = 42.12
+
+    print('The user {name} was born {age} years ago.'.format(name = txt, age = num))
+
+
+if __name__ == '__main__':
+    main()
+
+
+
