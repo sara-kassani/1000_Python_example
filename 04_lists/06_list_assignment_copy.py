3c4b6e62e25d997181bdb1e1b430c8c95bf09f86
sara-kassani <kassani.sara@gmail.com>
2021-08-23 07:28:04 -0600

    list assignment

---
+++ 04_lists/06_list_assignment_copy.py
@@ -0,0 +1,18 @@
+"""
+List assignment and list copy
+- There is one list in the memory and two pointers to it
+- I you really want to make a copy th pythonic way is to use the slice syntax
+- It creates a shallow copy.
+"""
+
+
+def main():
+    x = ['apple', 'bob', 'cat', 'drone']
+    y = x
+    x[0] = 'qqrq'
+    print(x)            # ['qqrq', 'bob', 'cat', 'drone']
+    print(y)            # ['qqrq', 'bob', 'cat', 'drone']
+
+
+if __name__ == '__main__':
+    main()
