9b16b9fbf7799842e431d40395816f9aa2c65be5
sara-kassani <kassani.sara@gmail.com>
2021-08-24 19:40:24 -0600

    for loop on lists

---
+++ 04_lists/10_list_for_loops.py
@@ -0,0 +1,15 @@
+"""
+for loop on lists
+"""
+def main():
+    things = ['apple', 'banana', 'peach', 42]
+    for var in things:
+        print(var)                          # apple
+                                            # banana
+                                            # peach
+                                            # 42
+
+
+if __name__ == '__main__':
+    main()
+
