ef2cd20a758e91968026244c2dc939fb1c9ed651
sara-kassani <kassani.sara@gmail.com>
2021-08-24 19:45:42 -0600

    list append

---
+++ 04_lists/14_list_append.py
@@ -0,0 +1,15 @@
+"""
+list append
+"""
+
+
+def main():
+    names = ['Joe', 'Kim', 'Jane', 'Bob', 'Kim']
+    print(names)
+
+    names.append('Nick')
+    print(names)                    # ['Joe', 'Kim', 'Jane', 'Bob', 'Kim', 'Nick']
+
+
+if __name__ == '__main__':
+    main()
