3c6dd7ec7cdbb8c5976de47a096b752917fff2d5
Sara Kassani <kassani.sara@gmail.com>
2021-09-27 11:46:25 -0700

    range

---
+++ 04_lists/33_range.py
@@ -0,0 +1,32 @@
+"""
+range
+"""
+
+
+def main():
+    for i in range(11, 18, 2):
+        print(i)
+
+    for i in range(5, 7):
+        print(i)
+
+    for i in range(3):
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
+
+# 11
+# 13
+# 15
+# 17
+#
+#
+# 5
+# 6
+#
+#
+# 0
+# 1
+# 2
