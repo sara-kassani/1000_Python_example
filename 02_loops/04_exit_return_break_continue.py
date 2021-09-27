2f51fbef6f2a6a59c1e7642cdd8b5a7c73e46a09
Sara Kassani <kassani.sara@gmail.com>
2021-09-26 21:19:47 -0700

    Update 04_exit_return_break_continue.py

--- 02_loops/04_exit_return_break_continue.py
+++ 02_loops/04_exit_return_break_continue.py
@@ -2,6 +2,6 @@
 Exit vs return vs break and continue
 • exit: will stop your program no matter where you call it.
 • return: will return from a function (it will stop the specific function only)
-• break:will stop the current “while” or “for” loop
+• break: will stop the current “while” or “for” loop
 • continue: will stop the current iteration of the current “while” or “for” loop
 """
