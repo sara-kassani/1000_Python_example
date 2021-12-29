36b05013cd6468123d1ac9829ed54c0294dcb1e1
sara-kassani <kassani.sara@gmail.com>
2021-12-28 19:10:06 -0800

    chnged the variable name

--- 08_sets/02_creating_emty_set.py
+++ 08_sets/02_creating_emty_set.py
@@ -9,8 +9,8 @@ Created on Tue Dec 21 03:01:56 2021
 
 def main():
 
-    objects = set()
-    print(objects)    #  set()
+    celestional_objects = set()
+    print(celestional_objects)    #  set()
 
 if __name__ == '__main__':
     main()
