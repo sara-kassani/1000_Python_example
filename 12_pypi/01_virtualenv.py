ababfdeeca7ec8f8590e969319d40326be3e4080
sara-kassani <kassani.sara@gmail.com>
2022-01-02 10:41:23 -0800

    create virtual python

---
+++ 12_pypi/01_virtualenv.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  2 10:05:53 2022
+
+@author: sarak
+"""
+
+# The virtualenv command will create a copy of python in the given directory inside the current directory.
+
+"""
+virtualenv -p python3 venv3
+source venv3/bin/activate
+...
+deactivate
+
+"""
+
+# After source-ing the ‘activate’ file the PATH will include the local python with a local version of
+# pip and easy_install. This requires bash or zsh.
+
+
+
+# $ easy_install module_name
+
+# $ pip install package_name
+
+
+# Upgrade pip
+# • pip install –upgrade pip Will probably not work on Windows because file is in use…
+# • easy_install pip Will work on Windows as well.
