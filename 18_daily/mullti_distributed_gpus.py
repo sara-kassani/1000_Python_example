60769bc6b2f8b6ad47ef819c94587ebaf78b4e29
Sara Kassani <kassani.sara@gmail.com>
2022-06-12 09:53:10 -0700

    Update mullti_distributed_gpus.py

--- 18_daily/mullti_distributed_gpus.py
+++ 18_daily/mullti_distributed_gpus.py
@@ -1,1 +1,17 @@
 
+# Create a MirroredStrategy.
+strategy = tf.distribute.MirroredStrategy()
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
+# Open a strategy scope.
+with strategy.scope():
+  # Everything that creates variables should be under the strategy scope.
+  # In general this is only model construction & `compile()`.
+  model = Model(...)
+  model.compile(...)
+
+# Train the model on all available devices.
+model.fit(train_dataset, validation_data=val_dataset, ...)
+
+# Test the model on all available devices.
+model.evaluate(test_dataset)
