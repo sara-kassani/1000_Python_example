fbf40af97b282275efc94f7ed144762f87fe22b0
Sara Kassani <kassani.sara@gmail.com>
2022-06-12 10:42:31 -0700

    Update mullti_distributed_gpus.py

--- 18_daily/mullti_distributed_gpus.py
+++ 18_daily/mullti_distributed_gpus.py
@@ -1,4 +1,18 @@
+strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"], 
+                                           cross_device_ops=tf.distribute.HierarchicalCopyAllReduce())
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
 
+with strategy.scope():
+  # model construction & `compile()`.
+    model1 = sm.Unet('densenet121', input_shape=input_shape, encoder_weights=encoder_weights, activation= activation)
+#     model1.summary()     # Trainable params: 26,555,121 with densenet121 (26M)
+    model1.compile(optimizer = optimizer, loss = dice_loss, metrics = metrics)
+
+# ##########################################################################################################################
+
+
+
+# original documentation - from keras.io
 # Create a MirroredStrategy.
 strategy = tf.distribute.MirroredStrategy()
 print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
