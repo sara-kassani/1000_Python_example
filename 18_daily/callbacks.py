e2be6036eda0eae310abf1a88d3617f77b8858f9
Sara Kassani <kassani.sara@gmail.com>
2022-06-13 19:43:14 -0700

    Update callbacks.py

--- 18_daily/callbacks.py
+++ 18_daily/callbacks.py
@@ -1,1 +1,16 @@
+checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath='best_weights.h5',
+                                                monitor='val_loss',
+                                                verbose=1,
+                                                save_best_only=True,
+                                                save_weights_only=False,
+                                                mode='auto')
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                                  patience=10,
+                                                  verbose=1,
+                                                  mode='min')
+# #################################################################################################
+ callbacks= [checkpoint, early_stopping]
+# or
+callbacks= [ModelCheckpoint(file_path, verbose=1, save_best_only=True, save_weights_only=False)]
 
+model.fit(..., callbacks = callbacks)
