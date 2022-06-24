4eb4fa57154f531660605d09238589a6bddb9f63
Sara Kassani <kassani.sara@gmail.com>
2022-06-23 18:26:54 -0700

    Update daily_solution.py

--- 18_daily/daily_solution.py
+++ 18_daily/daily_solution.py
@@ -159,7 +159,18 @@ set_seed()
 
 ###########################################################################
 
-
+rows,cols=3,3
+fig=plt.figure(figsize=(10,10))
+for i in range(1,rows*cols+1):
+    fig.add_subplot(rows,cols,i)
+    img_path=train_files[i]
+    msk_path=mask_files[i]
+    img=cv2.imread(img_path)
+    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    msk=cv2.imread(msk_path)
+    plt.imshow(img)
+    plt.imshow(msk,alpha=0.4)
+plt.show()
 
 ###########################################################################
 
