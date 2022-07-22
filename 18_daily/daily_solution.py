85a60ab61da401361394612c96e336907f0dea5e
Sara Kassani <kassani.sara@gmail.com>
2022-07-22 15:07:54 -0700

    Update daily_solution.py

--- 18_daily/daily_solution.py
+++ 18_daily/daily_solution.py
@@ -265,6 +265,12 @@ with mirrored_strategy.scope():
 
 ###########################################################################
 
+def makedirs(path): 
+    try: 
+        os.makedirs(path) 
+    except OSError: 
+        if not os.path.isdir(path): 
+            raise
 
 
 ###########################################################################
