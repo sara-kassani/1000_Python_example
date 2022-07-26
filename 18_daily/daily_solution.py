690912b1c4446ff44045d7ee41d242587c1d4af5
Sara Kassani <kassani.sara@gmail.com>
2022-07-26 11:11:42 -0700

    Update daily_solution.py

--- 18_daily/daily_solution.py
+++ 18_daily/daily_solution.py
@@ -332,7 +332,39 @@ def data_generator(img_list, mask_list, batch_size):
 
 ###########################################################################
 
+'''-----------------Data Generator-----------------'''
+# https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
 
+class DataGenerator(tf.keras.utils.Sequence):
+
+    def __init__(self, img_paths, mask_paths, batch_size, n_classes):
+        self.x, self.y = img_paths, mask_paths
+        self.batch_size = batch_size
+        self.n_classes = n_classes
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def read_nifti(self, filepath):
+        volume = nib.load(filepath).get_fdata()
+        volume = np.array(volume)
+        return volume
+
+
+    def __getitem__(self, idx):
+
+        batch_x = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        batch_y = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+
+        image = [self.read_nifti(image_file) for image_file in batch_x]
+        image = np.array(image, dtype=np.float32)
+        image = tf.expand_dims(image, axis=-1)
+        
+        label = [self.read_nifti(mask_file) for mask_file in batch_y]
+        label = np.array(label, dtype=np.float32)
+        label = tf.keras.utils.to_categorical(label, num_classes=self.n_classes)
+        
+        return image, label
 
 
 ###########################################################################
