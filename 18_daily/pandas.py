40398babce6a577f9f28eab30daec679d9f0bc87
Sara Kassani <kassani.sara@gmail.com>
2022-08-02 18:07:14 -0700

    Update pandas.py

--- 18_daily/pandas.py
+++ 18_daily/pandas.py
@@ -1,3 +1,8 @@
 # convert a df column into a numpy array (.iloc and .values)
 column_data= df.iloc[:, :].values     # the dataframe has only one column
 column_data
+###########################################################################
+pd.set_option('max_colwidth', -1)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_rows', 1000)
+pd.set_option('display.max_columns', 1000)
