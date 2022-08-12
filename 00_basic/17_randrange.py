963448c89311f770094eb942761b98f225897fbb
Sara Kassani <kassani.sara@gmail.com>
2022-08-12 08:43:03 -0700

    Update 17_randrange.py

--- 00_basic/17_randrange.py
+++ 00_basic/17_randrange.py
@@ -2,8 +2,8 @@
 import random
 def main():
     # mu= 1, sigma= 6
-    print(1 + int(6 * random.random() ))
-    print(random.randrange(1, 7))
+    print(1 + int(6 * random.random() ))   # 4
+    print(random.randrange(1, 7))          # 3
 
 if __name__ == '__main__':
     main()
