679ec55277e5905162e7e020e881ec8324b99e85
Sara Kassani <kassani.sara@gmail.com>
2022-09-17 15:20:05 -0700

    Update 02_access_list.py

--- 04_lists/02_access_list.py
+++ 04_lists/02_access_list.py
@@ -8,12 +8,14 @@ def main():
     planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn']
     print(planets)
 
-    print(planets[0])
-    print(type(planets[0]))
-    print(planets[3])
+    print(planets[0])     # Mercury
+    print(type(planets[0]))    # <class 'str'>
 
-    print(planets[0:1])
-    print(type(planets[0:1]))
+    print(planets[0:1])    # Mercury
+    print(type(planets[0:1]))    # <class 'list'>
+    
+    
+    
     print(planets[0:2])
     print(planets[1:3])
 
