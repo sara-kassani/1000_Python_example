db5664def72331249b8daf9515c15d3adfef214e
Sara Kassani <kassani.sara@gmail.com>
2022-11-29 19:52:53 -0800

    Create 03_remove_element.py

---
+++ leetcode/array/03_remove_element.py
@@ -0,0 +1,6 @@
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The relative order of the elements may be changed.
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+"""
